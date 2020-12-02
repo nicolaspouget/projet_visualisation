52e792a3699d251533cba5857f5adb4b81580159
Nicolas <nicolaspouget@orange.fr>
2020-12-02 14:07:43 +0100

    First push

---
+++ script/networkGraph.js
@@ -0,0 +1,246 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ * .data(graph.links.filter((d) => d.target.country === "Japan" && d.source.country === "Japan"))
+ * .data(graph.nodes.filter((d) => d.country === "Japan"))
+ * .force("collide", d3.forceCollide(r))
+ */
+
+/* global d3 */
+function callNetforkFunction(){
+    if(nameC!="") {
+        networkFunction(nameC);
+    }
+}
+
+function clearNetwork(){
+    d3.select("#networkContains").selectAll("svg").remove();
+}
+
+function networkFunction(country) {
+
+    d3.select("#staticBackdropLabel").text("Répartition des artistes associés à leur genre par labels pour le pays : "+country);
+
+    d3.select("#loader").attr("hidden", null);
+
+    let allData = [];
+    let nodesTable = [];
+    let linksTable = [];
+
+
+
+    d3.json("wasabi-artist.json").then(function (graph) {
+
+        function updatePays(pays) {
+            graph.forEach((item, index) => {
+                var labels = item.labels.replace("[", "").replace("]", "").replace("\"", "").replace("\"", "").replace("\",\"", ",").split(",");
+                var genres = item.genres.replace("[", "").replace("]", "").replace("\"", "").replace("\"", "").replace("\",\"", ",").split(",");
+
+
+                if (item.location.country == pays) {
+                    labels.forEach((label, index) => {
+                        label = label.replace("\"", "").replace("\"", "");
+                        if (label !== "") {
+                            if (!nodesTable.find(n => n.name == label && n.country == item.location.country)) {
+                                let noeudlabel = new Object();
+                                noeudlabel.name = label;
+                                noeudlabel.nbArtist = 1;
+                                noeudlabel.country = item.location.country;
+                                noeudlabel.category = 1;
+                                nodesTable.push(noeudlabel);
+                            } else {
+                                let noeud = nodesTable.find(n => n.name == label);
+                                noeud.nbArtist = noeud.nbArtist + 1;
+                            }
+
+                            if (!linksTable.find(n => n.source == label && n.target == item.name)) {
+                                let noeudlien1 = new Object();
+                                noeudlien1.source = label;
+                                noeudlien1.target = item.name;
+                                linksTable.push(noeudlien1);
+                            }
+                        }
+                    });
+
+                    genres.forEach((genre, index) => {
+                        genre = genre.replace("\"", "").replace("\"", "");
+                        if (genre !== "") {
+                            if (!nodesTable.find(n => n.name == genre && n.country == item.location.country)) {
+                                let noeudlabel = new Object();
+                                noeudlabel.name = genre;
+                                noeudlabel.nbArtist = 1;
+                                noeudlabel.country = item.location.country;
+                                noeudlabel.category = 2;
+                                nodesTable.push(noeudlabel);
+                            } else {
+                                let noeud = nodesTable.find(n => n.name == genre && n.country == item.location.country);
+                                noeud.nbArtist = noeud.nbArtist + 1;
+                            }
+
+                            if (!linksTable.find(n => n.source == item.name && n.target == genre)) {
+                                let noeudlien1 = new Object();
+                                noeudlien1.source = item.name;
+                                noeudlien1.target = genre;
+                                linksTable.push(noeudlien1);
+                            }
+                        }
+                    });
+                    if (!nodesTable.find(n => n.name == item.name)) {
+                        let noeudlabel = new Object();
+                        noeudlabel.name = item.name;
+                        noeudlabel.nbArtist = 1;
+                        noeudlabel.country = item.location.country;
+                        noeudlabel.category = 3;
+                        nodesTable.push(noeudlabel);
+                    }
+                }
+            });
+        }
+
+        updatePays(country);
+
+        d3.select("#loader").attr("hidden", "true");
+
+
+        var width = 1000;
+        var height = 800;
+
+        var r = 3;
+        var color = d3.scaleOrdinal(d3.schemeSet2)
+            .domain([0, 3]);
+
+
+
+        const svg = d3.select("#networkContains")
+            .append("svg")
+            .attr("width", width)
+            .attr("height", height);
+
+
+        var simulation = d3.forceSimulation(nodesTable)
+            .force("x", d3.forceX(width / 2))
+            .force("y", d3.forceY(height / 2))
+            .force("link", d3.forceLink(linksTable).id(d => d.name))
+            .force("collide", d3.forceCollide(r))
+            .force("charge", d3.forceManyBody())
+            .force("center", d3.forceCenter(width / 2, height / 2));
+
+        var g = svg.append("g")
+            .attr("class", "everything");
+
+        var link = g.append("g")
+            .attr("stroke", "#999")
+            .attr("stroke-opacity", 0.6)
+            .selectAll("line")
+            .data(linksTable)
+            .join("line")
+            .attr("stroke-width", function (d) {
+                return d.nbArtist + 5;
+            });
+
+        var node = g.append("g")
+            .attr("stroke", "#fff")
+            .attr("stroke-width", 1.5)
+            .selectAll("circle")
+            .data(nodesTable)
+            .join("circle")
+            .attr("r", function (d) {
+                var rayon = 0;
+                if (d.category !== 3 && d.nbArtist > 2) {
+                    rayon = d.nbArtist / 2;
+                } else {
+
+                    rayon = d.nbArtist;
+                }
+                return rayon + 3;
+            })
+            .attr("fill", function (d) {
+                return color(d.category);
+            })
+            .on('mouseover.fade', fade(0.1))
+            .on('mouseout.fade', fade(1));
+
+        node.append("title")
+            .text(function (d) {
+                var result = "";
+                if (d.category === 1) {
+                    result = "Label : " + d.name + "\n" + " Nombre d'artistes : " + d.nbArtist ;
+                }
+                if (d.category === 2) {
+                    result = "Genre : " + d.name + "\n" + " Nombre d'artistes : " + d.nbArtist ;
+                }
+                if (d.category === 3) {
+                    result = "Artiste : " + d.name ;
+                }
+                return result;
+            });
+
+        simulation.on("tick", () => {
+            link.attr("x1", d => d.source.x)
+                .attr("y1", d => d.source.y)
+                .attr("x2", d => d.target.x)
+                .attr("y2", d => d.target.y);
+
+            node.attr("cx", d => d.x)
+                .attr("cy", d => d.y);
+        });
+
+        var drag_handler = d3.drag()
+            .on("start", dragstarted)
+            .on("drag", dragged)
+            .on("end", dragended);
+
+        drag_handler(node);
+
+        function dragstarted() {
+            if (!d3.event.active)
+                simulation.alphaTarget(0.3).restart();
+            d3.event.subject.fx = d3.event.subject.x;
+            d3.event.subject.fy = d3.event.subject.y;
+        }
+
+        function dragged() {
+            d3.event.subject.fx = d3.event.x;
+            d3.event.subject.fy = d3.event.y;
+        }
+
+        function dragended() {
+            if (!d3.event.active)
+                simulation.alphaTarget(0);
+            d3.event.subject.fx = null;
+            d3.event.subject.fy = null;
+        }
+
+        var zoom_handler = d3.zoom()
+            .on("zoom", zoom_actions);
+
+        zoom_handler(svg);
+
+        function zoom_actions() {
+            g.attr("transform", d3.event.transform)
+        }
+
+        //highlight
+        const linkedByIndex = {};
+        linksTable.forEach(function (d) {
+            linkedByIndex[`${d.source.index},${d.target.index}`] = 1;
+        });
+
+        function isConnected(a, b) {
+            return linkedByIndex[`${a.index},${b.index}`] || linkedByIndex[`${b.index},${a.index}`] || a.index === b.index;
+        }
+
+        function fade(opacity) {
+            return function (d) {
+                node.style('stroke-opacity', function (o) {
+                    const thisOpacity = isConnected(d, o) ? 1 : opacity;
+                    this.setAttribute('fill-opacity', thisOpacity);
+                    return thisOpacity;
+                });
+
+                link.style('stroke-opacity', o => (o.source === d || o.target === d ? 1 : opacity));
+            };
+        }
+    });
+}
---
+++ script/visualisation.js
@@ -0,0 +1,330 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+var countriesValues = [];
+
+d3.json("CountryDecade.json").then(function(data) {
+    data.forEach(function(p){
+        countriesValues.push(p);
+    });
+});
+
+
+const width = 1000, height = 650;
+
+const svg = d3.select('#map').append("svg")
+    .attr("id", "svg")
+    .attr("width", width)
+    .attr("height", height)
+    .style("border", "solid 1px")
+    .style("border-color", "gray");
+
+/*
+ * .call(d3.zoom().on("zoom", function () {
+       svg.attr("transform", d3.event.transform);
+    }))
+ */
+
+
+
+const path = d3.geoPath();
+
+var projection = d3.geoMercator()
+    .center([0,40])                // GPS of location to zoom on
+    .scale(150)                       // This is like the zoom
+    .translate([ width/2, height/2 ]);
+    
+path.projection(projection);
+
+
+const deps = svg.append("g");
+
+
+// create a tooltip
+    var Tooltip = d3.select("#map")
+      .append("div")
+      .attr("class", "tooltip")
+      .style("opacity", 0)
+      .style("background-color", "#545f69")
+      .style("color", "white")
+      .style("border-width", "2px")
+      .style("border-radius", "5px")
+      .style("padding", "5px");
+      
+      // Three function that change the tooltip when user hover / move / leave a cell
+    var mouseover = function(d) {
+      Tooltip.style("opacity", 1);
+      d3.select(this).attr("fill-opacity", 1)
+          .attr("stroke", "#3c0036");
+    }
+    var mousemove = function(d) {
+        
+      Tooltip
+        .html(d.country+"<br>" + "Count :  "  + d.sum)
+        .style("left", (d3.mouse(this)[0]+200) + "px")
+        .style("top", (d3.mouse(this)[1]+90) + "px");
+
+    }
+    var mouseleave = function(d) {
+      Tooltip.style("opacity", 0);
+
+        d3.select(this).attr("fill-opacity", .6)
+            .attr("stroke", "#8d1e83");
+    }
+
+
+    // Add a scale for bubble size
+    var sizeAll = function(d){
+        if(d>6000){
+            d=10000;
+        }
+        return 2+(40*(d)/10000);
+    }
+
+    // Legend
+
+// Add legend: circles
+var valuesToShow = [1000, 5000, 20000]
+var xCircle = 75
+var xLabel = 225
+var yCircle = 625
+svg
+    .selectAll("legend")
+    .data(valuesToShow)
+    .enter()
+    .append("circle")
+    .attr("cx", xCircle)
+    .attr("cy", function(d){ return yCircle - sizeAll(d) } )
+    .attr("r", function(d){ return sizeAll(d) })
+    .style("fill", "none")
+    .attr("stroke", "black")
+
+// Add legend: segments
+svg
+    .selectAll("legend")
+    .data(valuesToShow)
+    .enter()
+    .append("line")
+    .attr('x1', function(d){ return xCircle + sizeAll(d) } )
+    .attr('x2', xLabel)
+    .attr('y1', function(d){ return yCircle - sizeAll(d) } )
+    .attr('y2', function(d){ return yCircle - sizeAll(d) } )
+    .attr('stroke', 'black')
+    .style('stroke-dasharray', ('2,2'))
+
+// Add legend: labels
+svg
+    .selectAll("legend")
+    .data(valuesToShow)
+    .enter()
+    .append("text")
+    .attr('x', xLabel)
+    .attr('y', function(d){ return yCircle - sizeAll(d) } )
+    .text( function(d){ return d } )
+    .style("font-size", 10)
+    .attr('alignment-baseline', 'middle')
+
+
+
+
+    // create a list of keys
+    var keys = ["Pop", "Rock", "Hip Hop", "Indie Rock", "Alternative Rock", "Folk", "Others"]
+
+    // Usually you have a color scale in your chart already
+    var color = d3.scaleOrdinal()
+        .domain(keys)
+        .range(d3.schemeSet1);
+
+    // Add one dot in the legend for each name.
+    var size = 20
+    svg.selectAll("mydots")
+        .data(keys)
+        .enter()
+        .append("rect")
+        .attr("x", 50)
+        .attr("y", function(d,i){ return 350 + i*(size+5)}) // 100 is where the first dot appears. 25 is the distance between dots
+        .attr("width", size)
+        .attr("height", size)
+        .style("opacity", 1)
+        .style("fill", function(d){ return color(d)})
+
+    // Add one dot in the legend for each name.
+    svg.selectAll("mylabels")
+        .data(keys)
+        .enter()
+        .append("text")
+        .attr("x", 50 + size*1.2)
+        .attr("y", function(d,i){ return 350 + i*(size+5) + (size/2)}) // 100 is where the first dot appears. 25 is the distance between dots
+        .style("fill", function(d){ return color(d)})
+        .style("opacity", 1)
+        .text(function(d){ return d})
+        .attr("text-anchor", "left")
+        .style("alignment-baseline", "middle")
+
+
+var nameC = "";
+
+
+d3.json('geoGenre.json').then(function(geojson) {
+
+    
+    // Map creation
+    deps.selectAll("path")
+        .data(geojson.features)
+        .enter()
+        .append("path")
+        .attr("d", path)
+        .style('fill', function(d){
+
+            var i = 0;
+            var indiceK = 0;
+            keys.forEach(function(k){
+                if (k == d.genre){
+                    indiceK = i;
+                }else{
+                    i++;
+                }
+            })
+
+            return color(keys[indiceK]);
+
+        })
+        .attr("stroke", "#F2F2F2")
+        .attr("stroke-width", 1)
+        .style("opacity", 0.5)
+        .on("click", function(d) {
+            nameC = d.properties.name;
+            d3.select("#countryChoiced").text(nameC);
+            d3.select("#changeVisu").attr("hidden", null);
+        })
+        .on("mouseover", function(d) {
+            d3.select(this).style("opacity", 1);
+        })
+        .on("mouseleave", function(d) {
+            d3.select(this).style("opacity", 0.5);
+        }).append("title").text(d => d.genre);
+
+
+
+    // Circles creation
+    var circles = svg
+        .selectAll("myCircles").data(geojson.features)
+        .data(countriesValues)
+        .enter().append('circle')
+        .attr("class" , function(d){ val = "Decade"+d.decade; return val })
+        .attr("cx", function(d) { return projection([d.longitude, d.latitude])[0];
+        })		 
+        .attr("cy", function(d) { return projection([d.longitude, d.latitude])[1];
+        })
+        .style("fill", "D700C4")
+        .style("opacity", 0).attr("r", 0)
+        .attr("stroke", "#8d1e83")
+        .attr("stroke-width", 1)
+        .attr("fill-opacity", .6)
+        .on("mouseover", mouseover)
+        .on("mousemove", mousemove)
+        .on("mouseleave", mouseleave)
+        .on("click", function(d) {
+            var nameC = d.properties.name;
+            console.log("test");
+            d3.select("#countryChoiced").text(nameC);
+            d3.select("#changeVisu").attr("hidden", null);
+        });
+
+
+        // Manage the displaying of the circles
+        function update(){
+            
+            // For each check box:
+            d3.selectAll(".checkbox").each(function(d){
+              var cb = d3.select(this);
+              var decade = cb.property("value");
+
+              var decadeChoosen = document.getElementById("currentDecadeValue").value;
+
+              var displayingDecade = d3.select("#displayingDecade");
+
+              // If the box is check, I show the group
+              if(!cb.property("checked")){
+                  
+                    displayingDecade.attr("hidden", "true");
+                  
+                    svg.selectAll(".Decade"+decade).transition().duration(1000).style("opacity", 1).attr("r", function(d){ return sizeAll(d.count) });
+
+                    // We hide all datas we don't need
+
+                    for(var i = 1880; i<2021; i+=10){
+                        
+                        svg.selectAll(".Decade"+i).style("opacity", 0).attr("r", 0);
+                    }
+
+                    // We display the displaying of te decades
+
+                    d3.select("#currentDecadeValue").attr("hidden","true");
+                    
+                    d3.select("#currentDecadeValue").attr("disabled","true");
+                    
+              // Otherwise I hide it
+              }else{
+                  
+                    displayingDecade.attr("hidden", null);
+                  
+                    displayingDecade.html("Décénie : "+decadeChoosen);
+                
+                   svg.selectAll(".Decade"+decade).transition().duration(1000).style("opacity", 0).attr("r", 0);
+                   
+                   
+                   svg.selectAll(".Decade"+decadeChoosen).transition().duration(1000).style("opacity", 1).attr("r", function(d){ return sizeAll(d.sum) });
+
+                  // We hide the displaying of the decade
+
+                  d3.select("#currentDecadeValue").attr("hidden",null);
+                   
+                    d3.select("#currentDecadeValue").attr("disabled",null);
+                   
+              }
+            });
+          }
+          
+          // When a button change, I run the update function
+            d3.selectAll(".checkbox").on("change",update);
+            
+            
+            
+          // When a button change, I run the update function
+            d3.selectAll("#currentDecadeValue").on("change",changeVal);
+            
+            function changeVal(){
+                
+                var decadeChoosen = document.getElementById("currentDecadeValue").value;
+                
+                var displayingDecade = d3.select("#displayingDecade");
+
+                // We hide all datas we don't need
+                for(var i = 1880; i<2021; i+=10){
+                    if(i!=decadeChoosen)
+                        svg.selectAll(".Decade"+i).style("opacity", 0).attr("r", 0);
+                }
+                
+                svg.selectAll(".Decade"+decadeChoosen).transition().duration(300).style("opacity", 1).attr("r", function(d){ return sizeAll(d.sum) });
+
+                // We hide the displaying of the decade
+
+                displayingDecade.attr("hidden", null);
+                
+                displayingDecade.html("Décénie : "+decadeChoosen);
+                
+            }
+
+
+            // And I initialize it at the beginning
+            update();
+
+});
+
+
+
+
